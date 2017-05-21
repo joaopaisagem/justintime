35a915d5414c9e85eb49d6c9831905bdcce1ea8a
Tiago Silva <a7809@alunos.ipca.pt>
2017-05-21 15:20:58 +0100

    Spring Security remove from pom xml WebSecuritymvc comment

--- src/main/java/pt/ipca/justintime/configs/WebSecurityConfig.java
+++ src/main/java/pt/ipca/justintime/configs/WebSecurityConfig.java
@@ -1,12 +1,13 @@
+/*
 package pt.ipca.justintime.configs;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,14 +28,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
-                .anyRequest().permitAll();
-    }
+                .antMatchers("/resources*/
+/**").permitAll()
+ .anyRequest().permitAll();
+ }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }
-}
+ @Autowired
+ public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+ auth
+ .inMemoryAuthentication()
+ .withUser("user").password("password").roles("USER");
+ }
+ }*/
